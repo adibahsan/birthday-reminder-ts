b8630293b18e435dfdb751e66d7b3e7ea848519f
Adib <adibahsan@iut-dhaka.edu>
2022-03-30 23:13:52 +0600

    added & imported dummy data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import data from './data/data';
+
+
+console.log(data);
 function App() {
   return (
     <div className="App">
