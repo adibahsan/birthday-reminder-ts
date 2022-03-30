cef161348772600ebd407f0f31b6096cfa18cd50
Adib <adibahsan@iut-dhaka.edu>
2022-03-30 23:46:54 +0600

    implemented the structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-
-import './App.css';
-import { BirthdayList } from './components/BirthdayList/BirthdayList';
-import data from './data/data';
-
-
+import { useState } from "react";
+import "./App.css";
+import { BirthdayList } from "./components/BirthdayList/BirthdayList";
+import data from "./data/data";
 
 console.log(data);
 function App() {
+  const [people, setPeople] = useState(data);
+  const [buttonTitle, setButtonTitle] = useState<string>("Clear All")
+  const buttonToggler = () =>{
+    people.length !== 0 ?
+    setPeople([]) : setPeople(data)
+  }
   return (
-   <main>
-     <h3>Birthday Lists</h3>
-     <BirthdayList people = {data}/> 
-   </main>
+    <main>
+      <section className ="container">
+        <h3>{people.length} people have birthday here</h3>
+        <BirthdayList people={people} />
+        <button onClick={buttonToggler}>{buttonTitle}</button>
+      </section>
+    </main>
   );
 }
 
